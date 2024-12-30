230ca40d7f7e1f9203adbd75dea95be6e87e84b7
Ahmed1680-6 <ahmed.tarek@meti.dev>
2024-12-30 19:23:06 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,10 @@
+name = "ahmed tarek"
+age = 22
+
+# What the user wants to learn in the project
+goals = "In this project, I want to learn the basics of Python programming, including working with variables, data types, and functions."
+
+# Print the introduction
+print("Hello! My name is " + name + ".")
+print("I am " + str(age) + " years old.")
+print("What I hope to learn in this project is: " + goals)
